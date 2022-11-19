bad66840722752361675bc497dbdf3767bbcc374
Nikita S <tanukofisimakin@gmail.com>
2022-11-19 07:17:18 +0300

    first?

---
+++ cogs/stats.py
@@ -0,0 +1,9 @@
+import disnake
+from disnake.ext import commands
+
+@commands.command()
+async def hello(ctx):
+    await ctx.send(f'Hello {ctx.author.display_name}.')
+
+def setup(bot):
+    bot.add_command(hello)
---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import disnake
+
+from disnake.ext import commnads
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = commands.Bot(command_prefix="*", intents=disnake.Intents.all())
+
+@bot.slash_command()
+async def test(interaction: disnake.AppCmdInter):
+    await interaction.send("ХУЙ")
+
+bot.load_extension("cogs.stats")
+
+TOKEN = os.environ["TOKEN"]
+bot.run=(TOKEN)
