c992f650ae82370c4cbec4a3b050b2ccf1321d79
Nikita S <tanukofisimakin@gmail.com>
2022-11-23 07:59:12 +0300

    update

--- cogs/stats.py
+++
@@ -1,9 +0,0 @@
-import disnake
-from disnake.ext import commands
-
-@commands.command()
-async def hello(ctx):
-    await ctx.send(f'Hello {ctx.author.display_name}.')
-
-def setup(bot):
-    bot.add_command(hello)
--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import os
 import disnake
-
 from disnake.ext import commands
-from dotenv import load_dotenv
 
-load_dotenv()
+bot = commands.Bot(command_prefix=("."), intents=disnake.Intents.all())
 
-bot = commands.Bot(command_prefix="*", intents=disnake.Intents.all())
+#  Load extension function in cog-file
+@bot.command()
+@commands.is_owner()
+async def load(ctx, extension):
+    bot.load_extension(f"cogs.{extension}")
 
-@bot.slash_command()
-async def test(interaction: disnake.AppCmdInter):
-    await interaction.send("ХУЙ")
+# Unload extension function in cog-file
+@bot.command()
+@commands.is_owner()
+async def unload(ctx, extension):
+    bot.unload_extension(f"cogs.{extension}")
 
-bot.load_extension("cogs.stats")
+# Reload extension function in cog-file
+@bot.command()
+@commands.is_owner()
+async def reload(ctx, extension):
+    bot.reload_extension(f"cogs.{extension}")
 
-TOKEN = os.environ["TOKEN"]
-bot.run=(TOKEN)
+# Check the cogs folder and the extension
+for filename in os.listdir("cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+
+bot.run = ("#")
