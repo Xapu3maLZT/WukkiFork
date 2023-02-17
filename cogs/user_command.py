6c174d006fe73e5b507a26f02b1a908467dc5211
Nikita Simakin <tanukofisimakin@gmail.com>
2023-02-17 11:03:20 +0300

    refactoring

--- cogs/user_command.py
+++ cogs/user_command.py
@@ -1,17 +1,11 @@
 import disnake
 from disnake.ext import commands
 
-class HelpCommand(commands.Cog):
+class Profile(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@commands.Cog.listener()
-async def on_ready(self):
-    print(f"Bot {self.bot.user} is ready to work")
 
-@commands.command()
-async def profile(self, ctx):
-    await ctx.reply("Test User command")
 
 def setup(bot):
     bot.add_cog(HelpCommand(bot))
