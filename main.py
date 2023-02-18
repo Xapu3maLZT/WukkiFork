ff9b9a6662631da8faf4e8d947488525dcecf638
Nikita Simakin <tanukofisimakin@gmail.com>
2023-02-18 07:52:30 +0300

    fuck start.bat my homies use a gitignore

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import json
 import disnake
 from disnake.ext import commands
 
-# Load settings from file
 with open('settings.json') as f:
     settings = json.load(f)
 
 bot = commands.Bot(command_prefix=(".",), intents=disnake.Intents.all())
 
-# Load extensions (cogs) from list in settings
 for extension in settings["cogs"]:
     bot.load_extension(extension)
 
